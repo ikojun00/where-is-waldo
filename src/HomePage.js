62c0adf8698115d41bc193ef0a1cccc3258af251
ikojun00 <ikojun00@fesb.hr>
2023-07-13 10:18:04 +0200

    feature: explaining the game

---
+++ src/HomePage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Jak from './images/jak.png';
+import Kratos from './images/kratos.png';
+import Ratchet from './images/ratchet.png';
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="homePageText">
+        <div className="homePageHeader">
+            Find these characters as fast as you can. Good luck!
+        </div>
+        <div className="homePageImages">
+            <div className='homePageChar'>
+                <img src={Jak} alt="Jak" />
+                <h3>Jak</h3>
+            </div>
+            <div className='homePageChar'>
+                <img src={Kratos} alt="Kratos" />
+                <h3>Kratos</h3>
+            </div>
+            <div className='homePageChar'>
+                <img src={Ratchet} alt="Ratchet" />
+                <h3>Ratchet</h3>
+            </div>
+        </div>
+        <div className="btnPlayNow">
+          <Link to="/game"><button>Play Now</button></Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
