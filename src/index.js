417931001b9dc828f921d41bd5824735ba01efcc
ikojun00 <ikojun00@fesb.hr>
2023-07-13 10:19:05 +0200

    adding fonts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+   google: {
+     families: ['Lato']
+   }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
