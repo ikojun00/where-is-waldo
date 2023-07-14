83245be4bd9b01e0a977cf409fd37236ba859926
ikojun00 <ikojun00@fesb.hr>
2023-07-14 12:11:32 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Game from "./Game";
+import HomePage from "./HomePage";
+import Leaderboard from "./Leaderboard";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    <Game />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
     </div>
   );
 }
