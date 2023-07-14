1e9170d5812be1e91c2d34fcd24479702ddbf529
ikojun00 <ikojun00@fesb.hr>
2023-07-14 12:10:33 +0200

    removing strict

--- src/index.js
+++ src/index.js
@@ -13,9 +13,7 @@ WebFont.load({
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>
 );
